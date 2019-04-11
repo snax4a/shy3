77449d24c41894d4b1e72e2ae6f45dd48e27cc37
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-11 12:31:04 -0400

    fix(footer): change a to router-link

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,15 +3,15 @@ footer
   .container
     .row
       .col-sm-5.col-md-7.col-lg-8.text-center.text-sm-left
-        a.btn.social(href='/classes', alt='Schedule for Yoga Classes in Squirrel Hill, East Liberty, and North Hills')
+        router-link.btn.social(to='/classes', alt='Schedule for Yoga Classes in Squirrel Hill, East Liberty, and North Hills')
           fa.fa-2x(icon='calendar-alt')
-        a.d-none.d-md-inline.schedule(href='/classes', alt='Class Schedule') Class Schedule
-        a.btn.social(href='/locations', alt='Directions to our Pittsburgh yoga schools')
+        router-link.d-none.d-md-inline.schedule(to='/classes', alt='Class Schedule') Class Schedule
+        router-link.btn.social(to='/locations', alt='Directions to our Pittsburgh yoga schools')
           fa.fa-2x(icon='map-marker-alt')
         span.d-none.d-lg-inline
-          a(href='/locations#squirrelhill', alt='Directions to Squirrel Hill school') Squirrel Hill
-          a(href='/locations#eastliberty', alt='Directions to East Liberty school') East Liberty
-          a(href='/locations#northhills', alt='Directions to North Hills school') North Hills
+          router-link(to='/locations#squirrelhill', alt='Directions to Squirrel Hill school') Squirrel Hill
+          router-link(to='/locations#eastliberty', alt='Directions to East Liberty school') East Liberty
+          router-link(to='/locations#northhills', alt='Directions to North Hills school') North Hills
         .spacer.d-block
       .col-sm-7.col-md-5.col-lg-4.text-center.text-sm-right
         a.btn.social(href='https://twitter.com/schoolhouseyoga', alt='Twitter', target='_blank') 
@@ -26,8 +26,8 @@ footer
     .row
       .col-md-6.text-center.text-md-left &copy; 2019 Schoolhouse Yoga, Inc. - All Rights Reserved
       .col-md-6.text-center.text-md-right
-        a(href='/privacy', alt='Privacy Policy') Privacy Policy
-        a(href='/terms', alt='Terms of use') Terms of Use
+        router-link(to='/privacy', alt='Privacy Policy') Privacy Policy
+        router-link(to='/terms', alt='Terms of use') Terms of Use
 </template>
 
 <script lang="ts">
