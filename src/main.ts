0d9ffc76430538276ed48506f8c7447a50b0a365
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-11 00:12:16 -0400

    chore(app): add scss, fontawesome SVGs

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,21 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import BootstrapVue from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faShoppingCart, faPhone, faMapMarkerAlt, faEnvelope, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import './scss/app.scss';
 
-Vue.config.productionTip = false;
+// FontAwesome 5.0 - load icons as SVG with tree-shaking
+library.add(faShoppingCart, faPhone, faEnvelope, faCalendarAlt, faMapMarkerAlt,
+  faTwitter, faFacebookF, faInstagram, faYoutube);
+Vue.component('fa', FontAwesomeIcon);
+
 Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
