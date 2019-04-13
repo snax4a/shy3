153f1c82ca610db064be46dea4c9c1b4ed07e99c
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-13 12:15:24 -0400

    chore(privacy,terms): implement

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,6 +1,41 @@
 <template lang='pug'>
 .container.privacy
-  h1 Privacy
+  h1 Privacy Policy
+  h4 General Information
+  p
+    | We collect the e-mail addresses of those who communicate with us via e-mail and the website, aggregate information on what pages consumers access or visit, and information volunteered by the consumer (such as survey information and/or site registrations). The information we collect is used to improve the content of our web pages and the quality of our service, and is not shared with or sold to other organizations for commercial purposes, except to provide products or services you&apos;ve requested, when we have your permission, or under the following circumstances:
+  ul
+    li
+      | It is necessary to share information in order to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the physical safety of any person, or as otherwise required by law.
+    li
+      | We transfer information about you if Schoolhouse Yoga is acquired by or merged with another company. In this event, Schoolhouse Yoga will notify you before information about you is transferred and becomes subject to a different privacy policy.
+  h4 Information Gathering and Usage
+  ul
+    li
+      | When you register for Schoolhouse Yoga, we ask for information such as your name, email address, billing address, or payment information.
+    li
+      | Schoolhouse Yoga uses collected information for the following general purposes: products and services provision, billing, identification and authentication, services improvement, contact, and research.
+  h4 Cookies
+  ul
+    li
+      | A cookie is a small amount of data, which often includes an anonymous unique identifier, that is sent to your browser from a web site&apos;s computers and stored on your computer&apos;s hard drive.
+    li Cookies are required to use the Schoolhouse Yoga website.
+    li
+      | We use cookies to record current session information, but do not use permanent cookies. You are required to re-login to your Schoolhouse Yoga account after a certain period of time has elapsed to protect you against others accidentally accessing your account contents.
+  h4 Data Storage
+  p
+    | Schoolhouse Yoga uses third-party vendors and hosting partners to provide the necessary hardware, software, networking, storage, and related technology required to run Schoolhouse Yoga&apos;s website. Although Schoolhouse Yoga owns the code, databases, and all rights to the Schoolhouse Yoga website or mobile apps, you retain all rights to your data.
+  h4 Disclosure
+  p
+    | Schoolhouse Yoga may disclose personally identifiable information under special circumstances, such as to comply with subpoenas.
+  h4 Changes
+  p
+    | Schoolhouse Yoga may periodically update this policy. We will notify you about significant changes in the way we treat personal information by sending a notice to the primary email address specified in your Schoolhouse Yoga primary account holder account or by placing a prominent notice on our site.
+  h4 Questions
+  p
+    | Questions about this Privacy Policy should be submitted through 
+    b Contact -> Send a message 
+    | above.
 </template>
 
 <script lang="ts">
--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,6 +1,81 @@
 <template lang='pug'>
 .container.terms
-  h1 Terms of Use
+  h1 Schoolhouse Yoga Terms of Service
+  p
+    | By using the Schoolhouse Yoga&apos;s web site (&quot;Service&quot;), or any services of Schoolhouse Yoga, Inc (&quot;SHY&quot;), you are agreeing to be bound by the following terms and conditions (&quot;Terms of Service&quot;). IF YOU ARE ENTERING INTO THIS AGREEMENT ON BEHALF OF A COMPANY OR OTHER LEGAL ENTITY, YOU REPRESENT THAT YOU HAVE THE AUTHORITY TO BIND SUCH ENTITY, ITS AFFILIATES AND ALL USERS WHO ACCESS OUR SERVICES THROUGH YOUR ACCOUNT TO THESE TERMS AND CONDITIONS, IN WHICH CASE THE TERMS &quot;YOU&quot; OR &quot;YOUR&quot; SHALL REFER TO SUCH ENTITY, ITS AFFILIATES AND USERS ASSOCIATED WITH IT. IF YOU DO NOT HAVE SUCH AUTHORITY, OR IF YOU DO NOT AGREE WITH THESE TERMS AND CONDITIONS, YOU MUST NOT ACCEPT THIS AGREEMENT AND MAY NOT USE THE SERVICES.
+  p
+    | If SHY makes material changes to these Terms, we will notify you by email or by posting a notice on our site before the changes are effective. Any new features that augment or enhance the current Service, including the release of new tools and resources, shall be subject to the Terms of Service. Continued use of the Service after any such changes shall constitute your consent to such changes. You can review the most current version of the Terms of Service at any time at:
+    br
+    |  
+    a(href='/terms') https://www.schoolhouseyoga.com/terms
+  p
+    | Violation of any of the terms below will result in the termination of your Account. While SHY prohibits such conduct and Content on the Service, you understand and agree that SHY cannot be responsible for the Content posted on the Service and you nonetheless may be exposed to such materials. You agree to use the Service at your own risk.
+  h4 Account Terms
+  ul
+    li You must be 18 years or older to use this Service.
+    li
+      | You must be a human. Accounts registered by &quot;bots&quot; or other automated methods are not permitted.
+    li
+      | You must provide your name, a valid email address, and any other information requested in order to complete the signup process.
+    li
+      | Your login may only be used by one person - a single login shared by multiple people is not permitted. You may create separate logins for as many people as your plan allows.
+    li
+      | You are responsible for maintaining the security of your account and password. SHY cannot and will not be liable for any loss or damage from your failure to comply with this security obligation.
+    li
+      | You are responsible for all Content posted and activity that occurs under your account (even when Content is posted by others who have accounts under your account).
+    li
+      | You may not use the Service for any illegal or unauthorized purpose. You must not, in the use of the Service, violate any laws in your jurisdiction (including but not limited to copyright or trademark laws).
+  h4 Payment and Refund Terms
+  ul
+    li Classes and workshops are non-refundable.
+  h4 Cancellation and Termination
+  ul
+    li
+      | Accounts can be canceled by sending a request through the online contact form.
+    li
+      | All of your Content will be immediately deleted from the Service upon cancellation. This information can not be recovered once your account is cancelled.
+    li
+      | SHY, in its sole discretion, has the right to suspend or terminate your account and refuse any and all current or future use of the Service, or any other SHY service, for any reason at any time. Such termination of the Service will result in the deactivation or deletion of your Account or your access to your Account, and the forfeiture and relinquishment of all Content in your Account. SHY reserves the right to refuse service to anyone for any reason at any time.
+  h4 Modifications to the Services and Prices
+  ul
+    li
+      | SHY reserves the right at any time and from time to time to modify or discontinue, temporarily or permanently, the Service (or any part thereof) with or without notice.
+    li
+      | Prices of all Services are subject to change upon 30 days notice from us. Such notice may be provided at any time by posting the changes to the SHY Site (schoolhouseyoga.com) or the Service itself. SHY shall not be liable to you or to any third-party for any modification, price change, suspension or discontinuance of the Service.
+  h4 Copyright and Content Ownership
+  ul
+    li
+      | We claim no intellectual property rights over the information you provide to the Service. Your profile uploaded remain yours.
+    li
+      | The look and feel of the Service is copyright &copy;2018 Schoolhouse Yoga, Inc. All rights reserved. You may not duplicate, copy, or reuse any portion of the HTML/CSS, Javascript, or visual design elements or concepts without express written permission from SHY.
+  h4 General conditions
+  ul
+    li Your use of the Service is at your sole risk. The service is provided on and
+    | &quot;as is&quot; and &quot;as available&quot; basis.
+    li Support for SHY services is only available in English, via email.
+    li You understand that SHY uses third-party vendors and hosting partners to
+    | provide the necessary hardware, software, networking, storage, and related technology required to run the Service.
+    li You must not modify, adapt or hack the Service or modify another website so
+    | as to falsely imply that it is associated with the Service, SHY, or any other SHY service.
+    li You may not use SHY Pages in violation of SHY&apos;s trademark or other rights or
+    | in violation of applicable law.
+    li
+      | You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service without the express written permission by SHY.
+    li
+      | Verbal, physical, written or other abuse (including threats of abuse or retribution) of any SHY customer, employee, member, or officer will result in immediate account termination.
+    li
+      | You understand that the technical processing and transmission of the Service, including your Content, may be transferred unencrypted and involve (a) transmissions over various networks; and (b) changes to conform and adapt to technical requirements of connecting networks or devices.
+    li
+      | You must not upload, post, host, or transmit unsolicited email, SMSs, or &quot;spam&quot; messages.
+    li You must not transmit any worms or viruses or any code of a destructive nature.
+    li
+      | SHY does not warrant that (i) the service will meet your specific requirements, (ii) the service will be uninterrupted, timely, secure, or error-free, (iii) the results that may be obtained from the use of the service will be accurate or reliable, (iv) the quality of any products, services, information, or other material purchased or obtained by you through the service will meet your expectations, and (v) any errors in the Service will be corrected.
+    li
+      | You expressly understand and agree that SHY shall not be liable for any direct, indirect, incidental, special, consequential or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if SHY has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the service; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into through or from the service; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third-party on the service; (v) or any other matter relating to the service.
+    li
+      | The failure of SHY to exercise or enforce any right or provision of the Terms of Service shall not constitute a waiver of such right or provision. The Terms of Service constitutes the entire agreement between you and SHY and govern your use of the Service, superseding any prior agreements between you and SHY (including, but not limited to, any prior versions of the Terms of Service). You agree that these Terms of Service and Your use of the Service are governed under Pennsylvania law.
+    li
+      | Questions about the Terms of Service should be should be submitted through the online contact form.
 </template>
 
 <script lang="ts">
