82dfa7f87731122b0a8152e83a513bba47533410
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-13 12:15:55 -0400

    chore(login): add GoogleLogin

---
+++ src/components/GoogleLogin.vue
@@ -0,0 +1,33 @@
+<template lang='pug'>
+b-button(@click='login()', variant='warning', size='lg')
+  img.img-google(src='/img/google.svg')
+  |  Google Login
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class GoogleLogin extends Vue {
+  @Prop({ default: 'a workshop' }) private title!: string;
+  @Prop({ default: 'https://www.schoolhouseyoga.com/workshops' }) private url!: string;
+  login() {
+    console.log('Logged in');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.btn-warning:hover {
+  background-position: 0 -42px;
+}
+
+.img-google {
+  height: 21px;
+  width: 21px;
+  position: relative;
+  top: -1px;
+  left: -2px;
+  margin: 0;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,17 @@
 <template lang='pug'>
 .container.login
   h1 Login
+  GoogleLogin
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import GoogleLogin from '@/components/GoogleLogin.vue';
 
-@Component
+@Component({
+  components: {
+    GoogleLogin,
+  },
+})
 export default class Login extends Vue {}
 </script>
