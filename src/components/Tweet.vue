6973ea28c66d5c73bfdc211f22a9fcc32a38b7b8
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-13 00:30:15 -0400

    chore(tweet): create

---
+++ src/components/Tweet.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+a.twitter-share-button(:data-text='title', :data-url='url', data-size='large', href='https://twitter.com/intent/tweet') Tweet
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Tweet extends Vue {
+  @Prop({ default: 'a workshop' }) private title!: string;
+  @Prop({ default: 'https://www.schoolhouseyoga.com/workshops' }) private url!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.twitter-share-button {
+  width: 78px;
+  height: 26px;
+}
+</style>
--- src/views/Workshops.vue
+++ src/views/Workshops.vue
@@ -3,4 +3,17 @@
   h1 H1 Yoga Workshops in Pittsburgh
   h2 H2 This is a test
   p P Here's some text to view
+  Tweet(title='Moon Salutations Workshop', url='https://www.schoolhouseyoga.com/workshops')
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import Tweet from '@/components/Tweet.vue';
+
+@Component({
+  components: {
+    Tweet,
+  },
+})
+export default class Workshops extends Vue {}
+</script>
