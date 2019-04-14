d7e5da79e19bc2c40d1601b86257772702a7ad0a
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-14 10:28:51 -0400

    chore(vue.config): add moment-locales-webpack-plugin, purgecss-webpack-plugin and dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -7,5 +12,17 @@ module.exports = {
         `
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new MomentLocalesPlugin(),
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ])
+      })
+    ]
   }
 };
