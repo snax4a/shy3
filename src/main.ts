4e93adfc866c67366d479d8280fab8b8582219dc
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-05-07 09:09:25 -0400

    chore(fa): add icons

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ import BootstrapVue from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faShoppingCart, faPhone, faMapMarkerAlt,
   faEnvelope, faCalendarAlt, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faCalendarPlus } from '@fortawesome/free-regular-svg-icons';
+import { faCalendarPlus, faTrashAlt, faCreditCard } from '@fortawesome/free-regular-svg-icons';
 import { faTwitter, faFacebookF, faInstagram, faYoutube,
   faGoogle, faApple, faWindows } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -38,7 +38,8 @@ import './scss/app.scss';
 
 // FontAwesome 5.0 - load each icon as SVG with tree-shaking to minimize size
 library.add(faShoppingCart, faPhone, faEnvelope, faCalendarAlt, faMapMarkerAlt, faCalendarPlus,
-  faTwitter, faFacebookF, faInstagram, faYoutube, faUser, faGoogle, faApple, faWindows);
+  faTwitter, faFacebookF, faInstagram, faYoutube, faUser, faGoogle, faApple, faWindows,
+  faTrashAlt, faCreditCard);
 Vue.component('fa', FontAwesomeIcon);
 
 Vue.use(Vuelidate);
