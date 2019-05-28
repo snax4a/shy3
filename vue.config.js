8c90fc24e26a34993e85c53bc1d9bb0e52e95a44
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-05-28 18:00:48 -0400

    refactor(vue.config): comment unused components

--- vue.config.js
+++ vue.config.js
@@ -17,9 +17,9 @@ module.exports = {
           image: 'xlink:href',
           'b-img': 'src',
           'b-img-lazy': ['src', 'blank-src'],
-          'b-card': 'img-src',
-          'b-card-img': 'img-src',
-          'b-card-img-lazy': ['src', 'blank-src'],
+          // 'b-card': 'img-src',
+          // 'b-card-img': 'img-src',
+          // 'b-card-img-lazy': ['src', 'blank-src'],
           'b-carousel-slide': 'img-src',
           'b-embed': 'src'
         }
