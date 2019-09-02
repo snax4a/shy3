c940ab4bfffb532bb55eec6bb06d3464b6ebecea
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-09-02 13:17:06 -0400

    chore(Onsite): add

---
+++ src/views/Onsite.vue
@@ -0,0 +1,11 @@
+<template lang='pug'>
+.container.privacy
+  h1 Schoolhouse Yoga at your site
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Onsite extends Vue {}
+</script>
